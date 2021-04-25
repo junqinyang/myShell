b0be5f6d2ac2a6a8821f84a824a1e0716d0b7259
victor_young <junqinyang957@gmail.com>
2021-04-25 13:09:08 +0800

    Update myshelldeploy.sh
    add z plugin

--- myshelldeploy.sh
+++ myshelldeploy.sh
@@ -7,7 +7,7 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 sed -i 's#ZSH_THEME=".*"#ZSH_THEME="ys"#' ~/.zshrc
-sed -i 's#plugins.*#plugins=(git zsh-autosuggestions zsh-syntax-highlighting)#' ~/.zshrc
+sed -i 's#plugins.*#plugins=(git z zsh-autosuggestions zsh-syntax-highlighting)#' ~/.zshrc
 
 chsh -s /bin/zsh
 
